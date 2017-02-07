8dd44b5dcc21bb5508f9165391050bf497069bc7
DavidArppe <DavidArppe@localhost>
2017-02-07 16:05:16 -0500

    [ADD] Added existing project

---
+++ Assets/ClothingController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ClothingController : MonoBehaviour
+{
+    public bool isMale;
+    [Range(1, 6)]
+    public int hairdo = 1;
+    public Color shirtColor, hairColor, skinTone;
+    public GameObject maleHair, femaleHair;
+    public Sprite girlShirt, guyShirt;
+
+    [Range(0, 4)]
+    public int smile = 0;
+    public SpriteRenderer smileObject;
+    public Sprite[] smiles;
+    [Range(0, 3)]
+    public int sleeveLength;
+
+    void UpdateCharacter()
+    {
+        var shirtRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        shirtRenderer.sprite = isMale ? guyShirt : girlShirt;
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("shirt"))
+        {
+            var renderer = g.GetComponent<SpriteRenderer>();
+
+            renderer.color = (shirtColor);
+            if (g.name.Contains("arm"))
+            {
+                if (g.name.Contains(sleeveLength == 0 ? "NULL" : sleeveLength == 1 ? "shorter" : sleeveLength == 2 ? "short" : sleeveLength == 3 ? "long" : "NULL"))
+                    renderer.enabled = true;
+                else
+                    renderer.enabled = false;
+            }
+        }
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("hair"))
+        {
+            g.GetComponent<SpriteRenderer>().color = (hairColor);
+        }
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("skin"))
+        {
+            g.GetComponent<SpriteRenderer>().color = (skinTone);
+        }
+
+        var mhair = maleHair.GetComponentsInChildren<Transform>();
+        var fhair = femaleHair.GetComponentsInChildren<Transform>();
+        for (int i = 1; i < mhair.Length; i++)
+        {
+            if (int.Parse(mhair[i].name) != hairdo) // This is really hardcoded, but it's easy for me right now
+            {
+                mhair[i].gameObject.GetComponent<SpriteRenderer>().enabled = (false);
+                fhair[i].gameObject.GetComponent<SpriteRenderer>().enabled = (false);
+            }
+            else
+            {
+                mhair[i].gameObject.GetComponent<SpriteRenderer>().enabled = (isMale ? true : false);
+                fhair[i].gameObject.GetComponent<SpriteRenderer>().enabled = (isMale ? false : true);
+            }
+        }
+
+        smileObject.sprite = smiles[smile];
+    }
+
+    void Start()
+    {
+        UpdateCharacter();
+    }
+
+    void OnValidate()
+    {
+        UpdateCharacter();
+    }
+}
---
+++ Assets/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item : System.Object
+{
+    public string name;
+    [Multiline]
+    public string description;
+    public Texture2D sprite;
+}
+
+public class ItemController : MonoBehaviour
+{
+    public Item[] items;
+
+    void Update()
+    {
+
+    }
+}
