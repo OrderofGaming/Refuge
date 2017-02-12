70173f6570436338a0c7bd83b386517b8173764f
DavidArppe <DavidArppe@localhost>
2017-02-12 00:16:16 -0500

    [ADD] Item controller, with items in unity scene

---
+++ Assets/Items.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Items : MonoBehaviour {
+
+	[System.Serializable]
+	public class ItemContainer
+	{
+		public string title;
+		[Multiline]
+		public string description;
+		public int value;
+		public Sprite image;
+	}
+
+	[Header("Every item in game")]
+	public ItemContainer[] items;
+}
