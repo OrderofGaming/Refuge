49a797a071ea92dc83c668a7b089ee1000f63b56
DavidArppe <DavidArppe@localhost>
2017-02-12 01:12:55 -0500

    [DEL] Old ItemController.cs script [ADD] New item controller, has an initializer [ADD] Player inventory, and food-hygiene-clothing bars

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour {
+	public class ScoreInfo
+	{
+		public void Initialize()
+		{
+			Items ic = GameObject.FindObjectOfType<Items> ();
+
+			inventory.Clear ();
+
+			for (int i = 0; i < 3; i++) {
+				inventory.Add (ic.items [Random.Range (0, ic.items.Length)]);
+			}
+		}
+
+		public int money;
+		public List<Items.ItemContainer> inventory;
+
+		public bool governmentID = false;
+		public int jailTime = 0;
+
+		public int food = 0;
+		public int hygiene = 0;
+		public int clothes = 0;
+	}
+
+	public ScoreInfo[] playerScores;
+}
