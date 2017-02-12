032e128530ced4f341ed9f2e7e261eb6e4c0b3fe
DavidArppe <DavidArppe@localhost>
2017-02-11 22:06:42 -0500

    [ADD] Card deck prefab [ADD] Card can be generated, stored in the deck, and drawn.         When card is drawn, calls named methods. Scrambles deck on empty. [ADD] Player background story text files

---
+++ Assets/CardActions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardActions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void GoToJail()
+    {
+        Debug.Log("You jail bro");
+    }
+
+    public void GetMoney()
+    {
+        Debug.Log("Buy me a car bro");
+    }
+}
